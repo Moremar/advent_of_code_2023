c4e857648ef2d6c87c5bfce0c3e50480a4560aa5
Moremar <chevallier.th@gmail.com>
2023-12-16 17:46:30 +0900

    Day 16

--- day16/part2.py
+++ day16/part2.py
@@ -6,11 +6,11 @@ from part1 import parse, count_energized_tiles, UP, DOWN, LEFT, RIGHT
 def solve(world):
     max_i = max([i for i,j in world])
     max_j = max([j for i,j in world])
-    starts = [((i, -1),      RIGHT) for i in range(max_i)] \
-           + [((i, max_j+1), LEFT)  for i in range(max_i)] \
-           + [((-1, j),      DOWN)  for j in range(max_j)] \
-           + [((max_i+1, j), UP)    for j in range(max_j)]
-    return max([count_energized_tiles(world, start) for start in starts])
+    initial_states = [((i, -1),      RIGHT) for i in range(max_i)] \
+                   + [((i, max_j+1), LEFT)  for i in range(max_i)] \
+                   + [((-1, j),      DOWN)  for j in range(max_j)] \
+                   + [((max_i+1, j), UP)    for j in range(max_j)]
+    return max([count_energized_tiles(world, state) for state in initial_states])
 
 
 if __name__ == "__main__":

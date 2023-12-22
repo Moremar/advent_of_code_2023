1ce08c27c1944f60beb3f3dc75088f22b12479a6
Moremar <chevallier.th@gmail.com>
2023-12-22 23:10:29 +0900

    Day 21

--- day21/part1.py
+++ day21/part1.py
@@ -1,67 +1,34 @@
 #!/usr/bin/env python3
-import re
 import time
-from collections import deque
-
-
-class Module:
-    def __init__(self, name, type, targets):
-        self.name = name
-        self.type = type
-        self.targets = targets
-        self.on = False          # only relevant when type is %
-        self.received = {}       # only relevant when type is &
-
-    def process(self, pulse, sender_name):
-        if self.type == '':
-            return pulse
-        elif self.type == '%' and pulse == 0:
-            self.on = not self.on
-            return 1 if self.on else 0
-        elif self.type == '&':
-            self.received[sender_name] = pulse
-            return 0 if all([self.received[x] == 1 for x in self.received]) else 1
-        return -1
 
 
 def parse(input_path):
     with open(input_path, 'r') as f:
-        modules = {}
-        for line in f.read().split('\n'):
-            type, name, targets = re.match(r'^([%&]?)(\w+) -> (.+)$', line).groups()
-            modules[name] = Module(name, type, targets.split(', '))
-        return modules
+        world, s_pos = set(), None
+        lines = f.read().split('\n')
+        for i in range(len(lines)):
+            for j in range(len(lines[0])):
+                if lines[i][j] == 'S':
+                    s_pos = (i, j)
+                if lines[i][j] in 'S.':
+                    world.add((i, j))
+        return world, s_pos
 
 
-def init_modules(modules):
-    names = [name for name in modules]  # must get it before the for loop as we create new modules in the loop
-    for module_name in names:
-        for target_name in modules[module_name].targets:
-            # create target if it does not have a rule for it (like 'output' in example or 'rx' in real input)
-            if target_name not in modules:
-                modules[target_name] = Module(target_name, 'x', [])  # type 'x' does nothing
-            # initialize the last received pulse for conjunction modules
-            if modules[target_name].type == '&':
-                modules[target_name].received[module_name] = 0
-
-
-def solve(modules):
-    init_modules(modules)
-    pulses_sent = {0: 0, 1: 0}
-    for i in range(1000):
-        to_process = deque([('broadcaster', 0, 'BUTTON')])
-        while len(to_process):
-            module_name, received_pulse, sender_name = to_process.popleft()
-            generated_pulse = modules[module_name].process(received_pulse, sender_name)
-            if generated_pulse != -1:
-                for target in modules[module_name].targets:
-                    pulses_sent[generated_pulse] += 1
-                    to_process.append((target, generated_pulse, module_name))
-    return pulses_sent[1] * (1000 + pulses_sent[0])
+def solve(world, s_pos):
+    reachable_pos = {s_pos}
+    for k in range(64):
+        positions = set()
+        for (i, j) in reachable_pos:
+            for p in [(i + 1, j), (i, j + 1), (i - 1, j), (i, j - 1)]:
+                if p in world:
+                    positions.add(p)
+        reachable_pos = positions
+    return len(reachable_pos)
 
 
 if __name__ == "__main__":
     start = time.time()
-    parsed = parse("data.txt")
-    result = solve(parsed)
+    world, s_pos = parse("data.txt")
+    result = solve(world, s_pos)
     print(f'Part 1 : {result}  ({round(1000 * (time.time() - start))} ms)')
--- day21/part2.py
+++ day21/part2.py
@@ -1,46 +1,55 @@
-#!/usr/bin/env python3
-import math
 import time
-from collections import deque
-from part1 import parse, init_modules
+from numpy.polynomial.polynomial import polyfit
+from part1 import parse
 
-# This part requires to observe the input file, and is possibly specific to my custom input file.
-# We notice that the 'rx' module can receive pulses only from the 'zh' module.
-# 'zh' is a conjunctive module, so it sends a high pulse only when the last pulse received from each sender is high.
-# Everytime a pulse is sent to 'rx', we observe the last pulse received by 'zh' from each of its senders.
-# We notice that each of its sender sends a high pulse periodically (every X button press).
-# The period is different for each of 'zh' module's senders.
-# Therefore, all senders will send a high pulse on the button press that is a common multiplier of all the periods.
-# So the fewest button press to reach this state is the LCM of all the periods of 'zh' module's senders.
+# We notice that the S is in the middle of a 131 x 131 grid, and there is no # in its row and column.
+# This means that in exactly 65 steps, we can reach the end of the pattern in all 4 directions.
+# Then every 131 steps further, we fill another pattern in each direction.
+# Since it propagates in 2D, the number of plots increases in X^2.
+# We try to find the function F such as F(x) = number of plots at step (x * 131 + 65)
+# We want to express it as F(x) = ax^2 + bx + c
+# For that, we calculate its value for x = 0, 1 and 2, and use the numpy polynomial solver to find the coefficients.
+# We notice that the target step is 26501365 = 65 + 202300 * 131, so the result is F(202300)
 
 
-def solve(modules):
-    init_modules(modules)
-    i = 0
-    periods = { sender_name: 0 for sender_name in modules['zh'].received }
-    while True:
-        i += 1
-        to_process = deque([('broadcaster', 0, 'BUTTON')])
-        while len(to_process):
-            module_name, received_pulse, sender_name = to_process.popleft()
-            generated_pulse = modules[module_name].process(received_pulse, sender_name)
-            if generated_pulse != -1:
-                for target in modules[module_name].targets:
-                    to_process.append((target, generated_pulse, module_name))
+def get_new_plots(world, to_check, prev_plots):
+    new_plots = set()
+    for (i, j) in to_check:
+        for (px, py) in [(i+1,j), (i, j+1), (i-1,j), (i, j-1)]:
+            if (px % 131, py % 131) in world and (px, py) not in prev_plots:
+                new_plots.add((px, py))
+    return new_plots
 
-            # when 'rx' receives a pulse, we check the last pulse received from each sender of the 'zh' module
-            # the first time it is a high pulse gives us the period for each sender
-            if 'rx' in modules[module_name].targets:
-                for zh_sender in periods:
-                    if periods[zh_sender] == 0 and modules['zh'].received[zh_sender] == 1:
-                        periods[zh_sender] = i
-                        if 0 not in periods.values():
-                            # we found the period for all senders
-                            return math.lcm(*periods.values())
+
+def find_quadratic_function(world, start):
+    curr, prev = {start}, set()
+    count_even, count_odd = 1, 0
+    F = []
+    for k in range(1, 65 + 2 * 131 + 1):
+        # only keep track of the latest plots that are reached at each step
+        curr, prev = get_new_plots(world, curr, prev), curr
+        if k % 2 == 0:
+            count_even += len(curr)
+        else:
+            count_odd += len(curr)
+
+        if k % 131 == 65:
+            F.append(count_even if k %2 == 0 else count_odd)
+
+    assert len(F) == 3
+    # we look for a degree 2 polynom F(x) = ax^2 + bx + c that fits
+    fitted = polyfit([0, 1, 2], F, 2)
+    a, b, c = round(fitted[2]), round(fitted[1]), round(fitted[0])
+    return lambda x: a * x * x + b * x + c
+
+
+def solve(world, start):
+    F = find_quadratic_function(world, start)
+    return F(202300)
 
 
 if __name__ == "__main__":
     start = time.time()
-    parsed = parse("data.txt")
-    result = solve(parsed)
+    world, s_pos = parse("data.txt")
+    result = solve(world, s_pos)
     print(f'Part 2 : {result}  ({round(1000 * (time.time() - start))} ms)')
